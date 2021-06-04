0d4f32524898395909552b51589f633d6136666e
cxl <1799359457@qq.com>
2021-06-04 08:38:55 +0800

    update

--- common/src/main/java/com/cxl/common/command/Commands.java
+++ common/src/main/java/com/cxl/common/command/Commands.java
@@ -3,5 +3,30 @@ package com.cxl.common.command;
 /**
  * @author cxl
  */
-public class Commands {
+public enum Commands {
+    /**
+     * 控制端或者傀儡端连接服务器时的命令
+     */
+    CONNECT,
+    /**
+     * 控制命令
+     */
+    CONTROL,
+    /**
+     * 鼠标事件
+     */
+    MOUSE_EVENT,
+    /**
+     * 键盘事件
+     */
+    KEYBOARD_EVENT,
+    /**
+     * 断开连接事件
+     */
+    CLOSE_EVENT,
+
+
+
+
+
 }
