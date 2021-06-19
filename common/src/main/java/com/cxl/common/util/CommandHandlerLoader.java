e1712a064b251f10ceb819aa7ea2562ead8faa00
cxl <1799359457@qq.com>
2021-06-19 22:49:17 +0800

    update

---
+++ common/src/main/java/com/cxl/common/util/CommandHandlerLoader.java
@@ -0,0 +1,11 @@
+package com.cxl.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author cxl
+ */
+public class CommandHandlerLoader {
+    private static final Logger LOGGER= LoggerFactory.getLogger(CommandHandlerLoader.class);
+}
