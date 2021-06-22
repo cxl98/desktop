edcda2fde6643b01def1ebeec545170004b2cc23
cxl <1799359457@qq.com>
2021-06-22 22:01:27 +0800

    update

--- common/src/main/java/com/cxl/common/util/CommandHandlerLoader.java
+++ common/src/main/java/com/cxl/common/util/CommandHandlerLoader.java
@@ -15,4 +15,30 @@ public class CommandHandlerLoader {
     private String name;
     private static final Logger LOGGER= LoggerFactory.getLogger(CommandHandlerLoader.class);
     private static final Map<Enum<Commands>, Handler> HANDLER=new ConcurrentHashMap<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Handler getCommandHandler(Enum<Commands> command){
+        if (null==command){
+            return null;
+        }
+        Handler handler = HANDLER.get(command);
+        if (handler == null) {
+            return null;
+        }
+        synchronized (CommandHandlerLoader.class){
+           Handler handler1= loadCommandHandler(command);
+           HANDLER.put(command,handler1);
+           return handler1;
+        }
+    }
+
+    private Handler loadCommandHandler(Enum<Commands> command) {
+        return null;
+    }
 }
